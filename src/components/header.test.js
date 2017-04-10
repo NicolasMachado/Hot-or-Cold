cc3743871c5e68156b18ff7ef5a2b8e9feb8dd9b
Nicolas Machado <nicoma63@gmail.com>
2017-04-10 15:18:29 +0200

    Test suite

---
+++ src/components/header.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount} from 'enzyme';
+
+import Header from './header';
+
+describe('<Header />', () => {
+    it('renders without crashing', () => {
+        shallow(<Header />);
+    });
+});
