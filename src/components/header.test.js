e2f37d7f8ff9f6ccc74e9f50d7a0031ae22f5d53
Nicolas Machado <nicoma63@gmail.com>
2017-04-13 11:38:45 +0200

    Test implementation

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -8,12 +8,4 @@ describe('<Header />', () => {
     it('renders without crashing', () => {
         shallow(<Header />);
     });
-/*
-    it('Should fire the onClick callback when the reset div is clicked', () => {
-        const callback = jest.fn();
-        const wrapper = mount(<Header onReset={callback} />);
-        wrapper.find('.new-game-link').simulate('click');
-        expect(callback).toHaveBeenCalled();
-    });
-    */
 });
